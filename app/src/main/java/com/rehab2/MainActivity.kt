9b595ccb2ce9a1532cfea5cc088d06d5698ecd2e
trsavecdusan-wq <trsavecdusan@gmail.com>
2026-05-11 23:32:27 +0100

    add AAC button click test

--- app/src/main/java/com/rehab2/MainActivity.kt
+++ app/src/main/java/com/rehab2/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.rehab2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnAacZejna).setOnClickListener {
+            Toast.makeText(this, it.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnAacLacna).setOnClickListener {
+            Toast.makeText(this, it.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnAacPomoc).setOnClickListener {
+            Toast.makeText(this, it.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnAacWc).setOnClickListener {
+            Toast.makeText(this, it.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnAacDobro).setOnClickListener {
+            Toast.makeText(this, it.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnAacSlabo).setOnClickListener {
+            Toast.makeText(this, it.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnAacUtrujena).setOnClickListener {
+            Toast.makeText(this, it.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnAacMraz).setOnClickListener {
+            Toast.makeText(this, it.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnAacVroce).setOnClickListener {
+            Toast.makeText(this, it.text.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
