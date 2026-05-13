5a74a6886182508a37911a3c400d69cbbf3eca6d
trsavecdusan-wq <trsavecdusan@gmail.com>
2026-05-13 18:27:01 +0100

    fix BuildConfig import in backup settings activity

--- app/src/main/java/com/rehab2/BackupSettingsActivity.kt
+++ app/src/main/java/com/rehab2/BackupSettingsActivity.kt
@@ -6,6 +6,7 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.rehab2.BuildConfig
 import com.rehab2.update.ApkDownloadManager
 import com.rehab2.update.GitHubUpdateClient
 
